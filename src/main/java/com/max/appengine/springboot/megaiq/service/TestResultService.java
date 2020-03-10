4955e0a48024aeab9e1483cb880aca22024a9bc4
maxsv0 <max.svistunov@gmail.com>
2020-03-10 21:21:42 +0100

    Update TestResultService.java

--- src/main/java/com/max/appengine/springboot/megaiq/service/TestResultService.java
+++ src/main/java/com/max/appengine/springboot/megaiq/service/TestResultService.java
@@ -20,7 +20,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.assertj.core.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
