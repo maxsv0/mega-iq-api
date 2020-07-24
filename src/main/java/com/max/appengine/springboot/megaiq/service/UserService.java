8870cb572fb147e2fd73dde8cb3cf83590cb31cf
maxsv0 <max.svistunov@gmail.com>
2020-07-24 23:29:27 +0200

    Fix for number of days

--- src/main/java/com/max/appengine/springboot/megaiq/service/UserService.java
+++ src/main/java/com/max/appengine/springboot/megaiq/service/UserService.java
@@ -64,11 +64,12 @@ public class UserService {
       currentPage = page.get();
     }
 
-    return loadUsersByPeriod(locale, 30, PageRequest.of(currentPage, LIMIT_LIST_PAGE));
+    // TODO: fix it later. for now 300 days
+    return loadUsersByPeriod(locale, 300, PageRequest.of(currentPage, LIMIT_LIST_PAGE));
   }
 
   public List<User> getUsersListTopToday(Locale locale) {
-    return loadUsersByPeriod(locale, 7, PageRequest.of(0, LIMIT_HOME_PAGE));
+    return loadUsersByPeriod(locale, 150, PageRequest.of(0, LIMIT_HOME_PAGE));
   }
 
   public User addUser(User user) throws MegaIQException, FirebaseAuthException {
