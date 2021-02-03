479273b589c155e2c08afdbd0283c5045417cc9d
Maksym Svistunov <max.svistunov@gmail.com>
2021-02-03 20:18:15 +0100

    Update TestController.java

--- src/main/java/com/max/appengine/springboot/megaiq/rest/TestController.java
+++ src/main/java/com/max/appengine/springboot/megaiq/rest/TestController.java
@@ -383,7 +383,7 @@ public class TestController extends AbstractApiController {
     List<PublicTestResult> listActive = new ArrayList<>();
 
     List<PublicTestResult> listResults = convertToPublicTestResult(
-            loadLatestResults(userLocale, PageRequest.of(0, 10, Sort.by(Sort.Order.desc("id")))));
+            loadLatestResults(userLocale, PageRequest.of(0, 10)));
 
     return sendResponsePublicTestResultList(listActive, listResults, this.testResultService.getResultCount(),
         userLocale);
@@ -397,7 +397,7 @@ public class TestController extends AbstractApiController {
     List<PublicTestResult> listActive = new ArrayList<>();
 
     List<PublicTestResult> listResults = convertToPublicTestResult(
-            loadLatestResults(userLocale, PageRequest.of(0, 50, Sort.by(Sort.Order.desc("id")))));
+            loadLatestResults(userLocale, PageRequest.of(0, 50)));
 
     return sendResponsePublicTestResultList(listActive, listResults, 0, userLocale);
   }
